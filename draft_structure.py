815fdf56f4047a9ebd67a1b5ea917c6b6b978c98
Shai Berger <shai@platonix.com>
2025-03-06 23:00:25 +0200

    Extract structure for CSV (for manual editing), part 2
    Drop questions in English (ASCII), they are filters
    Guess types for other questions

--- draft_structure.py
+++ draft_structure.py
@@ -10,7 +10,7 @@ import sys
 import yaml
 
 
-NO_SECTION = "-מחוץ לפרק-"
+NO_SECTION = "-שאלות נוספות-"
 
 
 def main(argv: list[str]):
@@ -33,18 +33,23 @@ def main(argv: list[str]):
 def guess_structure(reader):
     row1 = next(reader)
     row2 = next(reader)
+    data_rows = list(reader)  # Rest of rows
 
     if len(row1)!=len(row2):
         raise InvocationError(f"header in CSV file is malformed")
 
     structure = {NO_SECTION: {}}
-    for (t1, t2) in zip(row1, row2):
+    for col_no, (t1, t2) in enumerate(zip(row1, row2)):
         
         if not t2 or "Response" in t2:
             # Question is in row 1
             if t1 in structure[NO_SECTION]:
                 raise ValueError(f"Malformed CSV file: Two questions '{t1}'")
-            structure[NO_SECTION][t1] = None  # Mark as existing, type to be filled later
+            if t1.isascii():
+                # English marks technical fields
+                continue
+            responses = {row[col_no] for row in data_rows}
+            structure[NO_SECTION][t1] = guess_type_from_responses(responses)
             continue
         
         if t1:
@@ -61,13 +66,36 @@ def guess_structure(reader):
             structure[NO_SECTION].setdefault(t1, [])
             structure[NO_SECTION][t1].append(t2)
         else:
+            if t2.isascii():
+                # English marks technical fields
+                continue
             section = t1 or NO_SECTION
             structure.setdefault(section, {})
             if t2 in structure[section]:
                 raise ValueError(f"Two questions '{t2}' under '{t1}'")
-            structure[section][t2] = None  # type to be filled in
+            responses = {row[col_no] for row in data_rows}
+            structure[section][t2] = guess_type_from_responses(responses)
 
     return structure
+
+
+def guess_type_from_responses(responses: set):
+    # Guess question type from set of responses
+    responses.discard(None)
+    responses.discard("")
+    try:
+        # Numbers can appear as floats or ints, but come from CSV as strings
+        responses = {float(r) for r in responses}
+        if all(f==int(f) for f in responses):
+            responses = {int(f) for f in responses}
+    except ValueError:
+        pass
+    if responses <= {1, 2, 3}:
+        return "שלוש"
+    elif len(responses) <= 5:
+        return "חמש"
+    else:
+        return "טקסט"
 
 
 if __name__ == "__main__":
